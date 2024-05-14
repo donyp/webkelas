0f482a13506e3f5ace50d0fa20245052522d96e2
donyp <rylakito06@gmail.com>
2024-05-14 09:52:06 +0700

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,13 @@ import { getFirestore } from "firebase/firestore"
 import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmUIXJblRGF4HTCO3hOB8MykvLOq7whUE",
-  authDomain: "project-web-kelas.firebaseapp.com",
-  projectId: "project-web-kelas",
-  storageBucket: "project-web-kelas.appspot.com",
-  messagingSenderId: "202691312352", 
-  appId: "1:202691312352:web:e82e7a494e705b2a12ee85"
+  apiKey: "AIzaSyB2Bli9MRAu9ijlVKhhMkSab1Yct2w8GvY",
+  authDomain: "toko-mujib.firebaseapp.com",
+  projectId: "toko-mujib",
+  storageBucket: "toko-mujib.appspot.com",
+  messagingSenderId: "976376728819",
+  appId: "1:976376728819:web:9bcc5e4f4eb821884abb86",
+  measurementId: "G-H3JMZDPE6Q"
 
 
 };
